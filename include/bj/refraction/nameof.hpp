179cc9f0ba13d1bebcd2313b17aaa9844fe14a71
Beta Jester <betajester@betajester.com>
2021-04-07 15:01:21 +1000

    Improved nameof, cleanup, comments

---
+++ include/bj/refraction/nameof.hpp
@@ -0,0 +1,108 @@
+// Copyright (C) 2021 Glenn Duncan <betajester@betajester.com>
+// See README.md, LICENSE, or go to https://github.com/BetaJester/refraction
+// for details.
+
+#pragma once
+
+#include <algorithm>
+#include <string>
+#include <string_view>
+#include <version>
+
+namespace bj::inline v1 {
+
+// Sometimes you just have to use macros, they are undefined at the end of the file.
+#if defined(_MSC_VER)
+    #define FUNCNAME __FUNCSIG__
+#else 
+    // defined(__clang__) || defined(__GNUC__)
+    // assumed, maybe other odd compatible compilers.
+    #define FUNCNAME __PRETTY_FUNCTION__
+#endif
+
+#if defined(__cpp_lib_constexpr_string)
+    #define bjcxstring constexpr
+#else
+    #define bjcxstring
+#endif
+
+    namespace impl {
+
+        // Helper for detection of pretty function lengths.
+        class AAAdetectBBB {};
+
+        // Completely remove substrings from a string.
+        [[nodiscard]] inline bjcxstring void remove_substr(std::string &str, std::string_view substr) noexcept
+        {
+            std::string::size_type start{};
+            while (start != std::string::npos) {
+                start = str.find(substr);
+                if (start != std::string::npos) {
+                    str.erase(begin(str) + start, begin(str) + start + size(substr));
+                }
+            }
+        };
+
+        // Replace substrings in a string.
+        [[nodiscard]] inline bjcxstring void replace_substr(std::string &str, std::string_view subold, std::string_view subnew) noexcept
+        {
+            std::string::size_type start{};
+            while (start != std::string::npos) {
+                start = str.find(subold);
+                if (start != std::string::npos) {
+                    str.replace(start, size(subold), subnew);
+                }
+            }
+        };
+
+    } // namespace bj::inline v1::impl.
+
+    // Automatically get the positions from the start and end of the compiler
+    // string for a functions retty name to get the correct name.
+    // namesz() must have the same signiature length as nameof() for this to work.
+    template<typename T = impl::AAAdetectBBB>
+    [[nodiscard]] inline constexpr auto namesz() noexcept
+    {
+        std::string_view name{FUNCNAME};
+
+        auto start = name.find("class bj::v1::impl::AAA");
+        if (start == std::string_view::npos) {
+            start = name.find("bj::v1::impl::AAA");
+        }
+        auto finish = name.find("BBB") + 3;
+        auto end = name.size() - finish;
+
+        return std::pair{ start, end };
+    }
+
+    // Get the compiler specific string naming a type. Results can vary by
+    // compiler and standard library implementations.
+    template<typename T>
+    [[nodiscard]] inline constexpr auto nameof() noexcept
+    {
+        std::string_view name{FUNCNAME};
+        const auto bounds = namesz();
+        std::string_view::iterator a = name.begin() + bounds.first;
+        std::string_view::iterator b = name.end() - bounds.second;
+        return std::string_view(a, b);
+    }
+
+    // Get the string for a type name, cleaned of compiler specific oddities where
+    // they are identified.
+    template<typename T>
+    [[nodiscard]] inline bjcxstring std::string clean_nameof() noexcept
+    {
+        std::string name{ nameof<T>() };
+        impl::remove_substr(name, "struct ");
+        impl::remove_substr(name, "class ");
+        impl::remove_substr(name, "enum ");
+        impl::replace_substr(name, "long int", "long");
+        name.erase(std::remove(begin(name), end(name), ' '), end(name));
+        return name;
+    }
+
+// Clean house.
+#undef FUNCNAME
+#undef bjcxstring
+
+} // namespace bj::inline v1.
--- include/bj/refraction/refractor.hpp
+++ include/bj/refraction/refractor.hpp
@@ -11,76 +11,78 @@
 #include <unordered_map>
 #include <utility> // move.
 #include <cstdint> // uint32_t
-#include "nameit.hpp"
-
-// Implementation details, don't touch.
-namespace bj::impl::inline v1 {
-
-    // Adapted from https://stackoverflow.com/a/27950866/8612306 and comments.
-    // Gives a non-standard crc32 for a string.
-    [[nodiscard]] inline constexpr std::uint32_t crc32(std::string_view str) noexcept {
-        std::uint32_t crc = std::numeric_limits<std::uint32_t>::max();
-        for (auto c : str) {
-            crc ^= static_cast<unsigned char>(c);
-            for (std::size_t k{}; k < 8; ++k) {
-                crc = (crc >> 1) ^ (0x82F63B78 & (0 - (crc & 1)));
-            }
-        }
-        return crc;
-    }
-
-    // Wrapper for information about a refractor type.
-    template<typename Base>
-    struct make_return {
-        
-        // The return type for Base as a pointer type.
-        using type = std::unique_ptr<Base>;
-
-        // Returns a pointer type of the base class, for a new object of the derived class.
-        template<typename Derived>
-        [[nodiscard]] static type make() { return std::make_unique<Derived>(); }
-    };
-
-    // Specialization of the make_return type with no base class.
-    template<>
-    struct make_return<void> {
-        
-        // The return type for a class with no base type.
-        using type = void *;
-
-        // Returns a new object of a type, returned as a pointer with no type.
-        template<typename Class> [[nodiscard]] static type make() { return new Class; }
-    };
-
-    // Stores information related to a refractor with a base type.
-    template<typename Base>
-    struct refractor_info {
-        using return_info = make_return<Base>;
-        using make_object_f = typename return_info::type(*)();
-        std::uint32_t crc{};
-        std::string name;
-        make_object_f maker{ nullptr };
-    };
-
-    // A map of refractor into to crc "hashes". Due to C++ statics and templates,
-    // there is one produced per base class used.
-    template<typename Base>
-    struct map {
-
-        // The enclosed map type, may change.
-        using map_t = std::unordered_map<std::uint32_t, const refractor_info<Base> *>;
-
-        // Magic static for the enclosed map type.
-        [[nodiscard]] static map_t &get() {
-            static map_t map;
-            return map;
-        }
-    };
-
-} // namespace bj::impl.
-
+#include "nameof.hpp"
 
 namespace bj::inline v1 {
+
+    // Implementation details, don't touch.
+    namespace impl {
+
+        // Adapted from https://stackoverflow.com/a/27950866/8612306 and comments.
+        // Gives a non-standard crc32 for a string.
+        [[nodiscard]] inline constexpr std::uint32_t crc32(std::string_view str) noexcept
+        {
+            std::uint32_t crc = std::numeric_limits<std::uint32_t>::max();
+            for (auto c : str) {
+                crc ^= static_cast<unsigned char>(c);
+                for (std::size_t k{}; k < 8; ++k) {
+                    crc = (crc >> 1) ^ (0x82F63B78 & (0 - (crc & 1)));
+                }
+            }
+            return crc;
+        }
+
+        // Wrapper for information about a refractor type.
+        template<typename Base>
+        struct make_return
+        {
+            // The return type for Base as a pointer type.
+            using type = std::unique_ptr<Base>;
+
+            // Returns a pointer type of the base class, for a new object of the derived class.
+            template<typename Derived>
+            [[nodiscard]] static type make() { return std::make_unique<Derived>(); }
+        };
+
+        // Specialization of the make_return type with no base class.
+        template<>
+        struct make_return<void>
+        {
+            // The return type for a class with no base type.
+            using type = void *;
+
+            // Returns a new object of a type, returned as a pointer with no type.
+            template<typename Class> [[nodiscard]] static type make() { return new Class; }
+        };
+
+        // Stores information related to a refractor with a base type.
+        template<typename Base>
+        struct refractor_info
+        {
+            using return_info = make_return<Base>;
+            using make_object_f = typename return_info::type(*)();
+            std::uint32_t crc{};
+            std::string name;
+            make_object_f maker{ nullptr };
+        };
+
+        // A map of refractor into to crc "hashes". Due to C++ statics and templates,
+        // there is one produced per base class used.
+        template<typename Base>
+        struct map
+        {
+            // The enclosed map type, may change.
+            using map_t = std::unordered_map<std::uint32_t, const refractor_info<Base> *>;
+
+            // Magic static for the enclosed map type.
+            [[nodiscard]] static map_t &get()
+            {
+                static map_t map;
+                return map;
+            }
+        };
+
+    } // namespace bj::inline v1::impl
 
     // The return type of a refractor using a certain base.
     template<typename Base>
@@ -88,14 +90,16 @@ namespace bj::inline v1 {
     
     // Makes a pointer type of a base type to a new object of a derived type.
     template<typename Derived, typename Base> requires std::derived_from<Derived, Base> || std::is_void_v<Base>
-    [[nodiscard]] refract_return_t<Base> refract_make() {
+    [[nodiscard]] refract_return_t<Base> refract_make()
+    {
         return impl::refractor_info<Base>::return_info::template make<Derived>();
     }
 
     // Instantiate a refracted object from the list of types registered for a base type
     // using a registered crc "hash".
     template<typename Base>
-    [[nodiscard]] refract_return_t<Base> instantiate(const std::uint32_t crc) {
+    [[nodiscard]] refract_return_t<Base> instantiate(const std::uint32_t crc)
+    {
         const auto &map = impl::map<Base>::get();
         if (auto it = map.find(crc); it != map.end()) {
             return it->second->maker();
@@ -107,17 +111,20 @@ namespace bj::inline v1 {
     // Instantiate a refracted object from the list of types registered for a base type
     // using a registered name string.
     template<typename Base>
-    [[nodiscard]] refract_return_t<Base> instantiate(const std::string_view name) {
+    [[nodiscard]] refract_return_t<Base> instantiate(const std::string_view name)
+    {
         return instantiate<Base>(impl::crc32(name));
     }
 
     // Stores and builds information relating to a type into a list of types
     // registered for a specific base, or void.
     template<typename Derived, typename Base = void>
-    struct [[nodiscard]] refractor {
+    struct [[nodiscard]] refractor
+    {
 
         // Construct a refractor with a specific name.
-        refractor(std::string name) {
+        refractor(std::string name)
+        {
             info.crc = impl::crc32(name);
             info.name = std::move(name);
             info.maker = []() -> refract_return_t<Base> { return refract_make<Derived, Base>(); };
@@ -125,8 +132,9 @@ namespace bj::inline v1 {
         }
 
         // Construct a refractor with a discovered name.
-        refractor() {
-            info.name = bj::gettype<Derived>();
+        refractor()
+        {
+            info.name = bj::clean_nameof<Derived>();
             info.crc = impl::crc32(info.name);
             info.maker = []() -> refract_return_t<Base> { return refract_make<Derived, Base>(); };
             impl::map<Base>::get().emplace(info.crc, &info);
